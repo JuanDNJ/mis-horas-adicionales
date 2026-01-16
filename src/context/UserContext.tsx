ec372f9b50899ef15765406f19db7be61bd0502a
JuanDNJ <132897973+JuanDNJ@users.noreply.github.com>
2026-01-16 10:50:11 +0100

    feat: actualizar UserProvider con información de usuario predeterminada

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -3,7 +3,13 @@ import { UserContext } from ".";
 
 export const UserProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   return (
-    <UserContext.Provider value={{ displayName: "", photoURL: "", phoneNumber: "" }}>
+    <UserContext.Provider
+      value={{
+        displayName: "Juan Pérez",
+        photoURL: "http://example.com/photo.jpg",
+        phoneNumber: "640282614",
+      }}
+    >
       {children}
     </UserContext.Provider>
   );
