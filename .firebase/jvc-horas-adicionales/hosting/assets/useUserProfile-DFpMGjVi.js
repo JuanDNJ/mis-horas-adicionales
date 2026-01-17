c960cee01d1e3187dd44244ec4a6358fd6473735
JuanDNJ <132897973+JuanDNJ@users.noreply.github.com>
2026-01-17 09:41:35 +0100

    Refactor code structure and remove redundant code blocks for improved readability and maintainability

---
+++ .firebase/jvc-horas-adicionales/hosting/assets/useUserProfile-DFpMGjVi.js
@@ -0,0 +1,1 @@
+import{lt as e}from"./vendor-CuWPEnc8.js";import{s as t}from"./index-D4OOz8tB.js";var n=e();const r=()=>{let e=(0,n.useContext)(t);if(e===void 0)throw Error(`useUserProfile debe ser usado dentro de un ProfileProvider`);return e};export{r as t};
--- .firebase/jvc-horas-adicionales/hosting/assets/useUserProfile-UF8kLXuA.js
+++
@@ -1,9 +0,0 @@
-import { lt as e } from "./vendor-CuWPEnc8.js";
-import { s as t } from "./index-gFfXkDjW.js";
-var n = e();
-const r = () => {
-  let e = (0, n.useContext)(t);
-  if (e === void 0) throw Error(`useUserProfile debe ser usado dentro de un ProfileProvider`);
-  return e;
-};
-export { r as t };
