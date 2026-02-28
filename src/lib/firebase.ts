7969d023ee0e69ff616a5209c5a10b168f88a32d
Juan Antonio <juandevnjv@gmail.com>
2026-02-28 07:25:58 +0100

    refactor: update Firebase configuration and improve package dependencies
    - Updated the Vite dependency in package.json to version ^7.3.1.
    - Refactored Firebase initialization in firebase.ts to directly use environment variables.
    - Moved Firebase configuration to a new config file (src/config/index.ts) for better organization and maintainability.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -12,19 +12,16 @@ import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-console.log("Cargando configuración de Firebase desde variables de entorno...");
-const firebaseConfig = {
+// Initialize Firebase
+const firebaseApp = initializeApp({
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
   projectId: import.meta.env.VITE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID,
-};
-
-// Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
+});
 const firestore = getFirestore(firebaseApp);
 const storage = getStorage(firebaseApp);
 const auth = getAuth(firebaseApp);
